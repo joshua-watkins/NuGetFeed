bd7e3e954453b319ac8697e5c4ca819c562cb06b
Joshua Watkins <3676490+joshua-watkins@users.noreply.github.com>
2025-11-08 01:24:04 -0800

    Future website options

---
+++ src/NuGetFeed.Web/NuGetWebsiteOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetFeed;
+
+public class NuGetWebsiteOptions
+{
+    public const string DefaultSection = "NuGetServer";
+
+    // things like what frameworks pop up in the search box
+
+    // What the base color is?
+
+    // Idk other stuff
+}
