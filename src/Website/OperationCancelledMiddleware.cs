b271723d3c4d1f7eff737a7f87721df09f87b915
Joshua Watkins <3676490+joshua-watkins@users.noreply.github.com>
2025-11-13 08:43:27 -0800

    Modernizing for .NET 8

--- src/Website/OperationCancelledMiddleware.cs
+++ src/Website/OperationCancelledMiddleware.cs
@@ -7,50 +7,43 @@ using Microsoft.Extensions.Logging;
 namespace NuGetFeed;
 
 /// <summary>
-/// Captures <see cref="OperationCanceledException" /> and converts to HTTP 409 response.
+/// Captures <see cref="OperationCanceledException" /> and converts to HTTP 409 Conflict response.
 /// Based off: https://github.com/aspnet/AspNetCore/blob/28157e62597bf0e043bc7e937e44c5ec81946b83/src/Middleware/Diagnostics/src/DeveloperExceptionPage/DeveloperExceptionPageMiddleware.cs
 /// </summary>
-public class OperationCancelledMiddleware
+public class OperationCancelledMiddleware(RequestDelegate next, ILogger<OperationCancelledMiddleware> logger)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<OperationCancelledMiddleware> _logger;
-
-    public OperationCancelledMiddleware(RequestDelegate next, ILogger<OperationCancelledMiddleware> logger)
-    {
-        _next = next ?? throw new ArgumentNullException(nameof(next));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
+    private RequestDelegate Next { get; } = next ?? throw new ArgumentNullException(nameof(next));
+    private ILogger Logger { get; } = logger ?? throw new ArgumentNullException(nameof(logger));
 
     public async Task Invoke(HttpContext context)
     {
         try
         {
-            await _next(context);
+            await Next(context);
         }
         catch (Exception e) when (ShouldHandleException(context, e))
         {
             try
             {
-                _logger.LogWarning("Request cancelled");
+                Logger.LogWarning(e, "An operation was cancelled during the request.");
 
                 context.Response.Clear();
                 context.Response.StatusCode = (int)HttpStatusCode.Conflict;
                 return;
             }
-            catch (Exception)
-            {
-                // If there's an exception, rethrow the original exception.
+            catch
+            { // If there's an exception, rethrow the original exception.
             }
-
             throw;
         }
 
-        bool ShouldHandleException(HttpContext ctx, Exception e)
+        static bool ShouldHandleException(HttpContext context, Exception e)
         {
-            if (ctx.Response.HasStarted) return false;
+            if (context.Response.HasStarted)
+                return false;
 
-            if (e is OperationCanceledException) return true;
-            if (e.InnerException is OperationCanceledException) return true;
+            if (e is OperationCanceledException || e.InnerException is OperationCanceledException)
+                return true;
 
             return false;
         }
